567a0e120c4e4780ef731634be41668c307a96c7
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-02-07 19:12:56 -0300

    feat: implement filter by name for artists

--- backend/api.js
+++ backend/api.js
@@ -7,7 +7,17 @@ api.use(express.json())
 api.use(cors())
 
 api.get('/artists', (req, res) => {
-  res.status(200).json(artists)
+  const { name_like } = req.query
+
+  if (name_like) {
+    const filteredArtists = artists.filter(artist =>
+      artist.name.toLowerCase().includes(name_like.toLowerCase()) // filtragem de nomes
+    )
+    return res.json(filteredArtists)
+  }
+
+  // caso o parâmetro não seja fornecido, retorna todos os artistas
+  res.json(artists)
 })
 
 api.listen(3000, () => console.log('Server running...'))
